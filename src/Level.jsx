76f27a8af54a395c665823cd7b75c6757dff967f
Utente <Utente@email.com>
2023-07-06 11:41:20 +0200

    block position prop

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,12 +1,14 @@
-function BlockStart() {
-    return <mesh position={[0, -0.1, 0]} receiveShadow>
-        <boxGeometry args={[4, 0.2, 4]} />
-        <meshStandardMaterial color="crimson" />
-    </mesh>
+function BlockStart({position = [0, 0, 0]}) {
+    return <group position={position}>
+        <mesh position={[0, -0.1, 0]} receiveShadow>
+            <boxGeometry args={[4, 0.2, 4]} />
+            <meshStandardMaterial color="crimson" />
+        </mesh>
+    </group>
 }
 
 export default function Level() {
     return <>
-        <BlockStart />
+        <BlockStart position={[0, 0, 0]}/>
     </>
 }
