9eaed306a03b2f902a22c499673287f8ad9a5640
Utente <Utente@email.com>
2023-07-06 14:30:31 +0200

    block limbo

--- src/Level.jsx
+++ src/Level.jsx
@@ -15,9 +15,9 @@ function BlockStart({position = [0, 0, 0]}) {
     </group>
 }
 function BlockSpinner({position = [0, 0, 0]}) {
-    const obstacle = useRef()
     const [speed] = useState(() => (Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1))
-
+    
+    const obstacle = useRef()
     useFrame((state) => {
         const time = state.clock.getElapsedTime()
         const rotation = new THREE.Quaternion()
@@ -31,10 +31,27 @@ function BlockSpinner({position = [0, 0, 0]}) {
         </RigidBody>
     </group>
 }
+function BlockLimbo({position = [0, 0, 0]}) {
+    const [timeOffset] = useState(() => Math.random() * Math.PI * 2)
+    
+    const obstacle = useRef()
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime()
+        const y = Math.sin(time + timeOffset) + 1.15
+        obstacle.current.setNextKinematicTranslation({x:position[0], y: position[1] + y, z:position[2]})
+    })
+    return <group position={position}>
+        <mesh geometry={boxGeometry} material={floor2Material} position={[0, -0.1, 0]} scale={[4, 0.2, 4]} receiveShadow />
+        <RigidBody ref={obstacle} type="kinematicPosition" position={[0, 0.3, 0]} restitution={0.2} friction={0}>
+            <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[3.5, 0.3, 0.3]} castShadow receiveShadow/>
+        </RigidBody>
+    </group>
+}
 
 export default function Level() {
     return <>
-        <BlockStart position={[0, 0, 4]}/>
-        <BlockSpinner position={[0, 0, 0]} />
+        <BlockStart position={[0, 0, 8]}/>
+        <BlockSpinner position={[0, 0, 4]} />
+        <BlockLimbo position={[0, 0, 0]} />
     </>
 }
